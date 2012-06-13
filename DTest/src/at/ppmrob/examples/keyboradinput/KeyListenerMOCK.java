c77e265e2bd6d57c3c710ebb577dd319ce3789eb
Zefirot <tordaga@gmail.com>
2012-06-13 16:24:57 +0300

    self explanatory

--- DTest/src/at/ppmrob/examples/keyboradinput/KeyListenerMOCK.java
+++ DTest/src/at/ppmrob/examples/keyboradinput/KeyListenerMOCK.java
@@ -5,23 +5,64 @@ import java.awt.event.KeyListener;
 
 public class KeyListenerMOCK implements KeyListener {
 	
-	public KeyListenerMOCK(){
-		
+	private boolean front = false, back = false, left = false,
+			right = false, up = false, down = false;
+	
+	public KeyListenerMOCK(){}
+	
+	public int getfrontback()
+	{
+		if(front) return 1;
+		else if(back) return -1;
+		return 0;
+	}
+	
+	public int getleftright()
+	{
+		if(left) return 1;
+		else if(right) return -1;
+		return 0;
+	}
+	
+	public int getupdown()
+	{
+		if(up) return 1;
+		else if(down) return -1;
+		return 0;
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent k) {
+		// TODO Auto-generated method stub
+		if(k.getKeyCode() == KeyEvent.VK_DOWN)
+			back = true;
+		else if(k.getKeyCode() == KeyEvent.VK_UP)
+			front = true;
+		else if(k.getKeyCode() == KeyEvent.VK_LEFT)
+			left = true;
+		else if(k.getKeyCode() == KeyEvent.VK_RIGHT)
+			right = true;
+		else if(k.getKeyCode() == KeyEvent.VK_PLUS)
+			up = true;
+		else if(k.getKeyCode() == KeyEvent.VK_MINUS)
+			down = true;
 	}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
+	public void keyReleased(KeyEvent k) {
 		// TODO Auto-generated method stub
-
-		System.out.println(arg0.getKeyCode());
-	}
-
-	@Override
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		System.out.println(arg0.getKeyCode());
-		System.out.println("22222");
-	}
+		if(k.getKeyCode() == KeyEvent.VK_DOWN)
+			back = false;
+		else if(k.getKeyCode() == KeyEvent.VK_UP)
+			front = false;
+		else if(k.getKeyCode() == KeyEvent.VK_LEFT)
+			left = false;
+		else if(k.getKeyCode() == KeyEvent.VK_RIGHT)
+			right = false;
+		else if(k.getKeyCode() == KeyEvent.VK_PLUS)
+			up = false;
+		else if(k.getKeyCode() == KeyEvent.VK_MINUS)
+			down = false;	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
