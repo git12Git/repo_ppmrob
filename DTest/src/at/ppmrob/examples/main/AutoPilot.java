f9aea57157f438c96506b94367f9d169732e9238
Zefirot <tordaga@gmail.com>
2012-08-20 16:55:33 +0300

    updated autopilot including line following

--- DTest/src/at/ppmrob/examples/main/AutoPilot.java
+++ DTest/src/at/ppmrob/examples/main/AutoPilot.java
@@ -1,18 +1,36 @@
 package at.ppmrob.examples.main;
 
+import at.ppmrob.examples.FeatureDetection;
+import at.ppmrob.examples.MyCircle;
+import at.ppmrob.examples.MyLine;
+
 import com.codeminders.ardrone.ARDrone;
 import com.codeminders.ardrone.DroneVideoListener;
 import com.codeminders.ardrone.NavData;
 import com.codeminders.ardrone.NavDataListener;
+import com.googlecode.javacv.cpp.opencv_core.IplImage;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.lang.Math;
+import java.util.Vector;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class AutoPilot implements DroneVideoListener, NavDataListener{
 
 	private ARDrone drone;
 	public static double PI = 3.14159265;
-	
+	  private AtomicReference<BufferedImage> image          = new AtomicReference<BufferedImage>();
+	    FeatureDetection featureDetection = new FeatureDetection();
+	  
+	  
+	  IplImage iplImg_lines;
+	    IplImage iplImg_circles;
+	    Vector<MyLine> detectedLines;
+		Vector<MyCircle> detectedCircles;
+	    IplImage imgWithCircles;
+		IplImage imgWithLines;
+
 	@Override
 	public void navDataReceived(NavData nd) {
 		// TODO Auto-generated method stub
@@ -22,8 +40,23 @@ public class AutoPilot implements DroneVideoListener, NavDataListener{
 	@Override
 	public void frameReceived(int startX, int startY, int w, int h,
 			int[] rgbArray, int offset, int scansize) {
-		// TODO Auto-generated method stub
 		
+		BufferedImage im = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        im.setRGB(startX, startY, w, h, rgbArray, offset, scansize);
+      
+	       
+	        if(im != null)
+	        {
+	        	
+	        	int w1 = im.getWidth();
+	        	int h1 = im.getHeight();
+
+	        	iplImg_lines = IplImage.createFrom(im);
+	        	detectedLines = featureDetection.detectLines(iplImg_lines, w, h);
+
+	        	detectedCircles = featureDetection.detectCircles(imgWithLines, w, h);
+
+	        }
 	}
 
 	public AutoPilot(ARDrone drone) {
@@ -38,10 +71,16 @@ public class AutoPilot implements DroneVideoListener, NavDataListener{
 		
 		//Follow the lines
 		double averageangle = 0;
-		for(int i = 0; i < 1; i++)
+		for(int i = 0; i < detectedLines.size(); i++)
 		{
-			averageangle += lnangle(0, 0, 0, 0);
+
+			averageangle += lnangle(detectedLines.elementAt(i).point1.x(),
+					detectedLines.elementAt(i).point1.y(),
+					detectedLines.elementAt(i).point2.x(),
+					detectedLines.elementAt(i).point2.y());
 		}
+		averageangle /= detectedLines.size();
+
 		//Get the average of the line angles and go that way
 		
 		//Land
