d95a5ce8e7a8822ecb1ec53967c908177cd26095
slobo <slobopepsi@gmail.com>
2012-08-20 22:27:59 +0200

    3 new metdhods for autopilot

--- DTest/src/at/ppmrob/examples/main/AutoPilot.java
+++ DTest/src/at/ppmrob/examples/main/AutoPilot.java
@@ -12,150 +12,291 @@ import com.googlecode.javacv.cpp.opencv_core.IplImage;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.lang.Math;
 import java.util.Vector;
 import java.util.concurrent.atomic.AtomicReference;
 
-public class AutoPilot implements DroneVideoListener, NavDataListener{
+public class AutoPilot implements DroneVideoListener, NavDataListener {
 
-private ARDrone drone;
-public static double PI = 3.14159265;
-private AtomicReference<BufferedImage> image = new AtomicReference<BufferedImage>();
-FeatureDetection featureDetection = new FeatureDetection();
+	private ARDrone drone;
+	public static double PI = 3.14159265;
+	private AtomicReference<BufferedImage> image = new AtomicReference<BufferedImage>();
+	FeatureDetection featureDetection = new FeatureDetection();
 
+	private Point2D.Double averageCirclesCenter = new Point2D.Double();
+	
+	IplImage iplImg_lines;
+	IplImage iplImg_circles;
+	Vector<MyLine> detectedLines;
+	Vector<MyCircle> detectedCircles;
+	IplImage imgWithCircles;
+	IplImage imgWithLines;
 
-IplImage iplImg_lines;
-IplImage iplImg_circles;
-Vector<MyLine> detectedLines;
-Vector<MyCircle> detectedCircles;
-IplImage imgWithCircles;
-IplImage imgWithLines;
+	float left_tilt = -0.1f;
+	float right_tilt = 0.1f;
+	// forward NEED TO BE MINUS
+	float front_tilt = -0.1f;
+	float back_tilt = 0.1f;
 
-float left_tilt = -0.1f;
-float right_tilt = 0.1f;
-//forward NEED TO BE MINUS
-        float front_tilt = -0.1f;
-        float back_tilt = 0.1f;
+	@Override
+	public void navDataReceived(NavData nd) {
+		// TODO Auto-generated method stub
 
-@Override
-public void navDataReceived(NavData nd) {
-// TODO Auto-generated method stub
+	}
 
-}
+	@Override
+	public void frameReceived(int startX, int startY, int w, int h,
+			int[] rgbArray, int offset, int scansize) {
 
-@Override
-public void frameReceived(int startX, int startY, int w, int h,
-int[] rgbArray, int offset, int scansize) {
+		BufferedImage im = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+		im.setRGB(startX, startY, w, h, rgbArray, offset, scansize);
 
-BufferedImage im = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-        im.setRGB(startX, startY, w, h, rgbArray, offset, scansize);
-      
+		if (im != null) {
 
-if(im != null)
-{
+			int w1 = im.getWidth();
+			int h1 = im.getHeight();
 
-int w1 = im.getWidth();
-int h1 = im.getHeight();
+			iplImg_lines = IplImage.createFrom(im);
+			detectedLines = featureDetection.detectLines(iplImg_lines, w, h);
 
-iplImg_lines = IplImage.createFrom(im);
-detectedLines = featureDetection.detectLines(iplImg_lines, w, h);
+			detectedCircles = featureDetection
+					.detectCircles(imgWithLines, w, h);
 
-detectedCircles = featureDetection.detectCircles(imgWithLines, w, h);
+		}
+	}
 
-}
-}
+	// This method is when its NOT called by another class
+	public static void main(String[] args) {
+		ARDrone drone;
+		try {
+			// Create ARDrone object,
+			// connect to drone and initialize it.
+			drone = new ARDrone();
+			AutoPilot sophisticatedpilot = new AutoPilot(drone);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
 
-    //This method is when its NOT called by another class
-    public static void main(String[] args)
-    {
-        ARDrone drone;
-        try
-        {
-            // Create ARDrone object,
-            // connect to drone and initialize it.
-            drone = new ARDrone();
-            AutoPilot sophisticatedpilot = new AutoPilot(drone);
-        }
-        catch(Throwable e)
-        {
-            e.printStackTrace();
-        }
-    }
+	public AutoPilot(ARDrone drone) {
+		super();
+		this.drone = drone;
+		try {
 
-public AutoPilot(ARDrone drone) {
-super();
-this.drone = drone;
-try {
+			drone.connect();
+			drone.clearEmergencySignal();
 
-        drone.connect();
-        drone.clearEmergencySignal();
+			// Wait until drone is ready
+			drone.waitForReady(3000);
 
-            // Wait until drone is ready
-       drone.waitForReady(3000);
+			// do TRIM operation
+			drone.trim();
 
-            // do TRIM operation
-        drone.trim();
+			// Take off
+			drone.takeOff();
 
-//Take off
-drone.takeOff();
+			// Turn around 360 degrees
 
-//Turn around 360 degrees
+			// Follow the lines
+			int times = 0;
+			while (times < 100) {
+				double averageangle = 0;
+				for (int i = 0; i < detectedLines.size(); i++) {
 
-//Follow the lines
-int times = 0;
-while(times < 100)
-{
-double averageangle = 0;
-for(int i = 0; i < detectedLines.size(); i++)
-{
+					averageangle += lnangle(
+							detectedLines.elementAt(i).point1.x(),
+							detectedLines.elementAt(i).point1.y(),
+							detectedLines.elementAt(i).point2.x(),
+							detectedLines.elementAt(i).point2.y());
+				}
+				averageangle /= detectedLines.size();
+				if (averageangle > 60 && averageangle < 120)
+					drone.move(0.0f, front_tilt, 0.0f, 0.0f);
+				else if (averageangle <= 60)
+					drone.move(left_tilt, 0.0f, 0.0f, 0.0f);
+				else if (averageangle >= 120)
+					drone.move(right_tilt, 0.0f, 0.0f, 0.0f);
+				else
+					drone.land();
+				/*
+				 * this.addKeyListener(new KeyListener() { public void
+				 * keyTyped(KeyEvent e) {} public void keyReleased(KeyEvent e)
+				 * {} public void keyPressed(KeyEvent e) {}
+				 * 
+				 * });
+				 */
+				times++;
+			}
 
-averageangle += lnangle(detectedLines.elementAt(i).point1.x(),
-detectedLines.elementAt(i).point1.y(),
-detectedLines.elementAt(i).point2.x(),
-detectedLines.elementAt(i).point2.y());
-}
-averageangle /= detectedLines.size();
-if(averageangle > 60 && averageangle < 120)
-drone.move(0.0f, front_tilt, 0.0f, 0.0f);
-else if(averageangle <= 60)
-drone.move(left_tilt, 0.0f, 0.0f, 0.0f);
-else if(averageangle >= 120)
-drone.move(right_tilt, 0.0f, 0.0f, 0.0f);
-else drone.land();
-/*this.addKeyListener(new KeyListener() {
-public void keyTyped(KeyEvent e) {}
-public void keyReleased(KeyEvent e) {}
-public void keyPressed(KeyEvent e) {}
+			// Get the average of the line angles and go that way
 
-});*/
-times++;
-}
+			// Land
+			drone.land();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
 
-//Get the average of the line angles and go that way
+	public double lnangle(int x1, int y1, int x2, int y2) {
+		double angle = 0;
+		double div1 = (x2 - x1);
+		if (div1 == 0)
+			return 0;
+		double m1 = Math.atan((y2 - y1) / div1);
+		m1 *= 180 / PI;
 
-//Land
-drone.land();
-}
-catch (IOException e1) {e1.printStackTrace();}
-}
+		if (m1 < 0)
+			m1 = 360 + m1;
 
-public double lnangle(int x1, int y1, int x2, int y2)
-{
-double angle = 0;
-double div1 = (x2 - x1);
-if(div1 == 0) return 0;
-double m1 = Math.atan ((y2 - y1) / div1);
-m1 *= 180 / PI;
+		angle = m1;
+		if (angle < 0)
+			angle = 360 + angle;
+		if (angle > 180)
+			angle -= 180;
 
-if(m1 < 0) m1 = 360 + m1;
+		return angle;
+	}
 
-angle = m1;
-if(angle < 0) angle = 360 + angle;
-if(angle > 180) angle -= 180;
-
-return angle;
-}
-
+	
+	
+	
+	public Point2D.Double averageCenterOfFoundCircles(Vector<MyCircle> foundCircles) {
+		int circlesCount = 0;
+		int xCoordCount = 0;
+		int yCoordCount = 0;
+		
+		if(foundCircles!=null){
+			circlesCount=foundCircles.size();
+			if(circlesCount>0){
+				for(MyCircle circle_n:foundCircles){
+					xCoordCount+=circle_n.center.x();
+					yCoordCount+=circle_n.center.y();
+				}
+				averageCirclesCenter.setLocation((xCoordCount/circlesCount), (yCoordCount/circlesCount));
+				return averageCirclesCenter;
+			}
+		} 
+			//go back to bullseye ?!?!
+			averageCirclesCenter.setLocation(20, 80);
+			return averageCirclesCenter;
+		}
+	
+	/**
+	 * 
+	 *      height=144                height=144                height=144                   
+	 *     33%  33%                    33%  33%                  33%  33%
+	 *  +-------------+            +-------------+           +-------------+       
+	 * w|   || | ||   |100%       w|   || | ||   |100%      w|   || x ||   |100%  end          
+	 * i|   || | ||   |           i|   || | ||   |          i|   || | ||   |             
+	 * d|   || | ||   |66%        d|   || | ||   |66%       d|   || | ||   |66%           
+	 * t|   || | ||   |           t|   || | ||   |          t|   || | ||   |        
+	 * h|   || | ||   |33%        h|   || | ||   |33%       h|   || | ||   |33%          
+	 *  |   || x ||   | start      |   || | ||   |           |   || | ||   |      
+	 *  +-------------+            +-------------+           +-------------+      
+	 *   red green red              red green red            red green red     
+	 * 
+	 * @param averageCircCenter
+	 * @param w
+	 * @param h
+	 */
+	public void dronePleaseStayOver_Current_Bullseye(Point2D.Double averageCircCenter, int w, int h){
+		/**
+		 * ich bin nicht sicher aber ich glaube mich zu erinnern das bei der drone die width und height umkehrt
+		 * waren ??? (drone width == standard height   &   drone height == standard width)
+		 * w=176   33% = 58
+		 * h=144   33% = 47
+		 */
+		//
+		
+		Rectangle2D.Double greenZone_bullseye = new Rectangle2D.Double(h*0.33f, w, h*0.33f, w);
+		
+		Rectangle2D.Double redZone_bullseye_left_side = new Rectangle2D.Double(0, w, h*0.33f, w);
+		Rectangle2D.Double redZone_bullseye_right_side = new Rectangle2D.Double(h*0.66f, w, h*0.33f, w);
+		
+		if(!greenZone_bullseye.contains(averageCircCenter)){
+		    //TODO CIRCLE OUT OF SIGHT
+			//TODO drone go backward/forward
+			
+			int countMoves=0;
+			
+			
+			
+			
+			while(redZone_bullseye_left_side.contains(averageCircCenter)){
+		        // drone go right
+				try {
+					this.drone.move(0.1f, 0.0f, 0.0f, 0.0f);
+					Thread.sleep(700);
+					countMoves++;
+					if(countMoves>15){
+						this.drone.land();
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			
+			
+			while(redZone_bullseye_right_side.contains(averageCircCenter)){
+			   // drone go left
+				try {
+					this.drone.move(-0.1f, 0.0f, 0.0f, 0.0f);
+					Thread.sleep(700);
+					countMoves++;
+					if(countMoves>15){
+						this.drone.land();
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+	
+	/**
+	 *      height=144                height=144                height=144                   
+	 *     33%  33%                    33%  33%                  33%  33%
+	 *  +-------------+            +-------------+           +-------------+       
+	 * w|   || | ||   |100%       w|   || | ||   |100%      w|   || x ||   |100%  END          
+	 * i|   || | ||   |           i|   || | ||   |          i|   || | ||   |             
+	 * d|   || | ||   |66%        d|   || | ||   |66%       d|   || | ||   |66%           
+	 * t|   || | ||   |           t|   || | ||   |          t|   || | ||   |        
+	 * h|   || | ||   |33%        h|   || | ||   |33%       h|   || | ||   |33%          
+	 *  |   || x ||   | START      |   || | ||   |           |   || | ||   |      
+	 *  +-------------+            +-------------+           +-------------+      
+	 *   red green red              red green red            red green red     
+	 * 
+	 * @param averageCircCenter
+	 * @param w
+	 * @param h
+	 */
+	public void dronePleaseFollowTheLine(Point2D.Double averageCircCenter, int w, int h){
+		/**
+		 * ich bin nicht sicher aber ich glaube mich zu erinnern das bei der drone die width und height umkehrt
+		 * waren ??? (drone width == standard height   &   drone height == standard width)
+		 * w=176   33% = 58
+		 * h=144   33% = 47
+		 */
+		//
+		
+		Rectangle2D.Double greenZone_lines = new Rectangle2D.Double(h*0.33f, w, h*0.33f, w);
+		
+		Rectangle2D.Double redZone_lines_left_side = new Rectangle2D.Double(0, w, h*0.33f, w);
+		Rectangle2D.Double redZone_lines_right_side = new Rectangle2D.Double(h*0.66f, w, h*0.33f, w);
+	}
 }
